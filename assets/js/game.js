0cfca006b038de4a4bce8c97dc529f4a7558180f
Youssef <youssefkaroui6@gmail.comm>
2022-03-01 18:02:59 -0500

    n

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! JavaScript is running!");
+//window.alert("This is an alert! JavaScript is running!");
+function fight() {
+    window.alert("The fight has begun!");
+  }
+ // fight ();
+
+ var playerName= window.prompt("what is your robot's name?");
+window.alert(playerName);
+console.log(playerName);
+console.log(10 + 10);
+console.log("Our robot's name is" + playerName);
